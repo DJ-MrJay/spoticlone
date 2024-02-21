4ba1e66c811411f53490dec46479359c88eb5952
DJ-MrJay <djmrjay@gmail.com>
2024-02-21 16:56:24 +0300

    Update Search and Favs screens

--- app/(tabs)/search.tsx
+++ app/(tabs)/search.tsx
@@ -1,27 +1,54 @@
-import { StyleSheet } from 'react-native';
+import { FlatList, TextInput, View, Text, StyleSheet } from "react-native";
+import { tracks } from "../../assets/data/tracks";
+import TrackListItem from "@/components/TrackListItem";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { FontAwesome } from "@expo/vector-icons";
+import { useState } from "react";
 
-import EditScreenInfo from '@/components/EditScreenInfo';
-import { Text, View } from '@/components/Themed';
+export default function SearchScreen() {
+  const [search, setSearch] = useState("");
 
-export default function TabTwoScreen() {
   return (
-    <View style={styles.container}></View>
+    <SafeAreaView>
+      <View style={styles.header}>
+        {}
+        <FontAwesome name="search" size={16} color="gray" />
+        <TextInput
+          value={search}
+          onChangeText={setSearch}
+          placeholder="What do you want to listen to?"
+          placeholderTextColor="gray"
+          style={styles.input}
+        />
+        <Text onPress={() => setSearch("")} style={{ color: "gray" }}>
+          Cancel
+        </Text>
+      </View>
+
+      <FlatList
+        data={tracks}
+        renderItem={({ item }) => <TrackListItem track={item} />}
+        showsVerticalScrollIndicator={false}
+      />
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+  header: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    gap: 15,
+    padding: 10,
   },
-  // title: {
-  //   fontSize: 20,
-  //   fontWeight: 'bold',
-  // },
-  // separator: {
-  //   marginVertical: 30,
-  //   height: 1,
-  //   width: '80%',
-  // },
+
+  input: {
+    flex: 1,
+    backgroundColor: "#121314",
+    paddingVertical: 5,
+    paddingHorizontal: 10,
+    color: "white",
+    borderRadius: 5,
+  },
 });
