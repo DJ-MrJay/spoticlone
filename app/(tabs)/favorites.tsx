818de45ddec633a13bf95047ca4fa65c3036c814
DJ-MrJay <djmrjay@gmail.com>
2024-02-21 13:18:28 +0300

    Clean template

--- app/(tabs)/favorites.tsx
+++ app/(tabs)/favorites.tsx
@@ -1,7 +1,9 @@
 import { StyleSheet } from 'react-native';
+
+import EditScreenInfo from '@/components/EditScreenInfo';
 import { Text, View } from '@/components/Themed';
 
-export default function TabOneScreen() {
+export default function TabTwoScreen() {
   return (
     <View style={styles.container}></View>
   );
@@ -13,4 +15,13 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+//   title: {
+//     fontSize: 20,
+//     fontWeight: 'bold',
+//   },
+//   separator: {
+//     marginVertical: 30,
+//     height: 1,
+//     width: '80%',
+//   },
 });
