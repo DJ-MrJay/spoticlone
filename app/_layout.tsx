b9adb40453479c1134b6914a5f76e61c5ec5ac80
DJ-MrJay <djmrjay@gmail.com>
2024-02-23 00:15:23 +0300

    Setup Apollo Client

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -10,6 +10,7 @@ import * as SplashScreen from "expo-splash-screen";
 import { useEffect } from "react";
 import { useColorScheme } from "@/components/useColorScheme";
 import PlayerProvider from "@/providers/PlayerProvider";
+import ApolloClientProvider from "@/providers/ApolloClientProvider";
 
 export {
   // Catch any errors thrown by the Layout component.
@@ -53,12 +54,14 @@ function RootLayoutNav() {
 
   return (
     <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
-      <PlayerProvider>
-        <Stack>
-          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-          <Stack.Screen name="modal" options={{ presentation: "modal" }} />
-        </Stack>
-      </PlayerProvider>
+      <ApolloClientProvider>
+        <PlayerProvider>
+          <Stack>
+            <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+            <Stack.Screen name="modal" options={{ presentation: "modal" }} />
+          </Stack>
+        </PlayerProvider>
+      </ApolloClientProvider>
     </ThemeProvider>
   );
 }
---
+++ providers/ApolloClientProvider.tsx
@@ -0,0 +1,17 @@
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+import { PropsWithChildren } from "react";
+
+const client = new ApolloClient({
+  uri: "https://annaregina.stepzen.net/api/billowing-anteater/__graphql",
+  headers: {
+    Authorization:
+      "apikey annaregina::stepzen.net+1000::02f60e253cc079982893cabca53b64b9c8d9e0ea58bc8fb32cd9c1cec6dcfba7",
+  },
+  cache: new InMemoryCache(),
+});
+
+const ApolloClientProvider = ({ children }: PropsWithChildren) => {
+  return <ApolloProvider client={client}>{children}</ApolloProvider>;
+};
+
+export default ApolloClientProvider;
